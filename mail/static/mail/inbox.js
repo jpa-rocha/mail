fbf5926b8d4d75792ba6ae54645dbabc80958247
CptCaveira <janos.rocha@gmail.com>
2021-04-20 15:24:19 +0200

    added loading inbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Send email
+  document.querySelector('#compose-form').addEventListener('submit', post_mail);
 });
 
 function compose_email() {
@@ -16,11 +19,36 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-body').value = ''; 
+
 }
+
+function post_mail() {
+
+  const recipients = document.querySelector('#compose-recipients').value
+  const subject = document.querySelector('#compose-subject').value
+  const body = document.querySelector('#compose-body').value
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+  
+  return false
+}
+
 
 function load_mailbox(mailbox) {
   
@@ -30,4 +58,36 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load recieved mails
+  if (mailbox === 'inbox') {
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+      // Print emails
+      console.log(emails);
+      emails.forEach(email =>{
+          const element = document.createElement('div');
+          element.classList.add('email');
+          element.id = email.id
+          const body = document.createElement('div');
+          body.classList.add('body')
+          body.innerHTML = email.body
+          document.querySelector('#emails-view').append(element);
+          document.querySelector('#emails-view').append(body);
+          const sender = document.createElement('span');
+          sender.classList.add('sender');
+          const subject = document.createElement('span');
+          subject.classList.add('subject');
+          const timestamp = document.createElement('span');
+          timestamp.classList.add('timestamp');
+          sender.innerHTML = email.sender
+          subject.innerHTML = email.subject
+          timestamp.innerHTML = email.timestamp
+          document.getElementById(element.id).appendChild(sender);
+          document.getElementById(element.id).appendChild(subject);
+          document.getElementById(element.id).appendChild(timestamp);
+      })})
+  }
 }
+
